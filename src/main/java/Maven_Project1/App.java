abfa5737c24c582aacf8e4ba73646c4537be0b10
unknown <sudheerkumar2k@gmail.com>
2022-10-08 10:56:22 +0530

    MVN Project

---
+++ src/main/java/Maven_Project1/App.java
@@ -0,0 +1,13 @@
+package Maven_Project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
